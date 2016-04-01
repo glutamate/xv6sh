6c796f64e820ab345f891670a3c32d73eac0da74
Tom Nielsen <tanielsen@gmail.com>
2016-04-01 22:12:58 +0100

    basic interaction

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,30 @@
 module Main where
 
+import System.Console.Haskeline
+import Control.Monad.Trans.State.Strict
+import Control.Monad.IO.Class (liftIO)
+
+data ShSt = ShSt
+
+initShSt = ShSt
+
+type ShM a = InputT (StateT ShSt IO) a
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = flip evalStateT initShSt $ runInputT defaultSettings runShell
+
+chomp :: String -> String
+chomp = reverse . chomp' . reverse . chomp' where
+  chomp' (' ':s) = chomp' s
+  chomp' s = s
+
+runShell :: ShM ()
+runShell = do
+  mln <- getInputLine "$ "
+  case fmap chomp mln of
+    Just "exit" -> return ()
+    Nothing -> return ()
+    Just ln -> goLine (words ln) >> runShell
+
+goLine :: [String] -> ShM ()
+goLine s = liftIO $ putStrLn $ "you wrote: "++ unwords s
